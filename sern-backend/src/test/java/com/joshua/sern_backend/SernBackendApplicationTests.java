1ff8889de394d3431bc8c81f9cc1208259e30c05
eiufb <joshuarengifo@gmail.com>
2026-01-17 12:53:57 -0300

    Add files via upload

---
+++ sern-backend/src/test/java/com/joshua/sern_backend/SernBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshua.sern_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SernBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
